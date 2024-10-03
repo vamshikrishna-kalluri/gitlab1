e5ddad9075671e74c911c8d3ecdaedc06e6e9cf3
vamshikrishna-kalluri <vamshikalluri.cloud@gmail.com>
2024-10-03 04:06:39 +0000

    added content in branch

---
+++ file1.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#
+echo "please enter first number"
+read a
+echo "please enter second number"
+read b
+
+echo "Addition of a and b is $((a+b)) "
+
+echo "Substraction of a and b is $((a-b)) "
+
+echo "Multiplication  of a and b is $((a*b)) "
+
+echo "DIvision of a and b is $((a/b)) "
+
+echo "Modulus of a and b is $((a%b)) "
